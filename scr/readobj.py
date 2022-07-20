4f52170214a9375899eb2c75343174ae1677f2fc
admin <admin@TUNGVM-MACLAP.local>
2022-07-20 10:24:46 +0700

    Added some functionality for table parsing, testing on local data source.

--- scr/readobj.py
+++ scr/readobj.py
@@ -1,7 +1,9 @@
+from codecs import escape_encode
 import re
 import contextlib
+from numpy import true_divide
 import pandas as pd
-
+import copy
 
 class ParsingSQLOwnersAndObjects:
     """ Parses for the owner and object names from stored
@@ -18,29 +20,20 @@ class ParsingSQLOwnersAndObjects:
             self.stored_proc.close()
             self.stored_proc = None
         
-    
     def unique(self, ls):
-    
-        # initialize a null list
-        unique_list = []
+        result = list(dict.fromkeys(ls))
+        return result
 
-        # traverse for all elements
-        for x in ls:
-            # check if exists in unique_list or not
-            if x not in unique_list:
-                unique_list.append(x)
-
-        return unique_list
     
     
-    def clean_sql_string(self, string):
+    def split_sql_string(self, string):
         # separates large string into individual statements,
         # capitalises and removes all white space characters
         string = string.upper()
         string = re.sub("\s+", " ", string)
-        string = string.split(";")
+        ar_string = string.split(";")
         
-        return string
+        return ar_string
 
     def cross_search_for_name_and_index(self, ls, stmt):
         
@@ -50,13 +43,40 @@ class ParsingSQLOwnersAndObjects:
         
         for name in ls:
             if name in stmt:
-                start = stmt.find(name)
-                end = start + len(name)
-                output_list.append((name, start, end))
+                concat = [(name, m.start(), m.end()) for m in re.finditer(name, stmt)]
+                output_list += concat
                 
-        return output_list
+        return self.remove_overlap(output_list)
+
+    def remove_overlap(self, obj_found):
+        obj_found = sorted(obj_found, key=lambda tup: tup[1])
+        # print(obj_found)
+
+        def overlap(tuple1, tuple2, overlapThresh = 1):
+            len_overlap =min(tuple1[2], tuple2[2]) - max(tuple1[1], tuple2[1]) 
+
+            len_tup1 = tuple1[2]-tuple1[1]
+            len_tup2 = tuple2[2]-tuple2[1]
+            min_len = min(len_tup1, len_tup2)
+
+            if len_overlap / min_len == overlapThresh:
+                return tuple1 if len_tup1 < len_tup2 else tuple2
+            else: return False
+
+        obj_found_copy = copy.deepcopy(obj_found)
+        for id1, obj1 in enumerate(obj_found):
+            for id2, obj2 in enumerate(obj_found):
+                if id1 > id2 and overlap(obj1, obj2):
+                    a = overlap(obj1, obj2) 
+                    if a in obj_found_copy:
+
+                        obj_found_copy.remove(overlap(obj1, obj2))
+    
+        return obj_found_copy
+            
     
     def query_object_type(self, owner, obj, df):
+        #
         query_ = ("OWNER == '{}'  and OBJECT_NAME == '{}'").format(owner, obj)
         return df.query(query_)["OBJECT_TYPE"].tolist()
 
@@ -66,6 +86,7 @@ class ParsingSQLOwnersAndObjects:
         # reads file and store in variable
         # this txt file with stored procedures are separated with '$$'
         content = self.stored_proc.read()
+
         content = content.split("$$")
     
         #define lists to store unique owners and objects 
@@ -78,31 +99,46 @@ class ParsingSQLOwnersAndObjects:
         objects = self.database["OBJECT_NAME"].tolist()
         objects = self.unique(objects)
         
-        for stmt in content :
-            # cleans the statement to match format of strings in database
-            stmt = self.clean_sql_string(stmt)
-
-        for sql in content:
-            # searches for an owner name in each statement by cross referencing distinct owners list
-            owner_found = self.cross_search_for_name_and_index(owners, sql)
-            obj_found = self.cross_search_for_name_and_index(objects, sql)
-            
-           
-        # case dot between owner and object
-        # eg: SCHEMA.Table, CARS.Toyota
-        for owner in owner_found:
-            for obj in obj_found:
-                if owner[2] == obj[1] - 1:
-                    print(owner, obj)
-                    print(self.query_object_type(owner[0], obj[0], self.database))
+        
 
         
-        self.close_file()
+        for stmt in content :
+            # print(stmt)
+            
+            
+            # cleans the statement to match format of strings in database
+            stmt = self.split_sql_string(stmt) 
+            for st in stmt:
+                owner_found = self.cross_search_for_name_and_index(owners, st)
+                print(owner_found)
+                
+                obj_found = self.cross_search_for_name_and_index(objects, st)
+
+            # searches for an owner name in each statement by cross referencing distinct owners list
+            
+                
+                # case dot between owner and object
+                # eg: SCHEMA.Table, CARS.Toyota
+                for owner in owner_found:
+                    for obj in obj_found:
+                        if owner[2] == obj[1] - 1:
+                            print (owner, obj)
+                            print (self.query_object_type(owner[0], obj[0], self.database))
+
+
+            
         
         
 
 
             
 if __name__ == '__main__': # for testing on data source
-    a = ParsingSQLOwnersAndObjects("datascr/all_objects.csv", "datascr/new 2.txt")
+
+    file_all_objects = 'datascr/all_objects.csv'
+    file_sql_source = 'datascr/test.txt' 
+    a = ParsingSQLOwnersAndObjects(file_all_objects,file_sql_source)
     a.find_objects_in_sql_stmt()
+    
+
+
+    
